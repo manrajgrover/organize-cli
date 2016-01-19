b1179ce32fd1e25cb30d1264a40a0349fdab20fd
Manraj Singh <manrajsinghgrover@gmail.com>
2016-01-19 18:33:31 +0530

    File types added

---
+++ index.js
@@ -0,0 +1,7 @@
+var docs = ["DOC", "DOCX", "PPT", "PPTX", "PAGES", "PDF", "ODT", "ODP", "XLSX", "XLS", "ODS", "TXT", "IN", "OUT"];
+var compressed = ["RAR", "JAR", "ZIP", "TAR", "MAR", "ISO", "LZ", "7ZIP", "TGZ", "GZ", "BZ2"];
+var audio = ["MP3", "WAV", "WMA", "MKA", "AAC", "MID", "RA", "RAM", "RM", "OGG"];
+var video = ["FLV", "WMV", "MOV", "MP4", "MPEG", "3GP", "MKV"];
+var code = ["CPP", "RB", "PY", "HTML", "CSS", "JS"];
+var images = ["JPG", "JPEG", "GIF", "PNG", "SVG"];
+var sys_files = ["DEB", "EXE", "SH", "BUNDLE"];
