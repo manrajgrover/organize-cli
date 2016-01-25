d07412cee86ac5dc06341c2df4a2e0228e77bdcd
Manraj Singh <manrajsinghgrover@gmail.com>
2016-01-25 19:10:18 +0530

    Directories creation added

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 var fs = require('fs'), path = require('path');
 
-var getExtension = function(filename) {
+var getExtension = function (filename) {
 	var i = filename.lastIndexOf('.');
-	return (i < 0) ? '' : filename.substr(i);
+	return (i < 0) ? '' : filename.substr(i + 1);
 }
 
-var mkdir = function(path){
-    try{
-        fs.mkdirSync(path);
-    } catch(e){
-        if(e.code != 'EEXIST'){
-            throw e;
-        }
-    }
+var mkdir = function (path) {
+	try {
+		fs.mkdirSync(path);
+	} catch (e) {
+		if (e.code != 'EEXIST') {
+			throw e;
+		}
+	}
 }
 
 var audio = ["MP3", "WAV", "WMA", "MKA", "AAC", "MID", "RA", "RAM", "RM", "OGG"];
@@ -24,17 +24,28 @@ var sys_files = ["DEB", "EXE", "SH", "BUNDLE"];
 var video = ["FLV", "WMV", "MOV", "MP4", "MPEG", "3GP", "MKV"];
 
 var formats = {
-    "Music": audio,
-    "Codes": code,
-    "Compressed": compressed,
-    "Documents": docs,
-    "Images": images,
-    "System Files": sys_files,
-    "Video": video
+	"Music" : audio,
+	"Codes" : code,
+	"Compressed" : compressed,
+	"Documents" : docs,
+	"Images" : images,
+	"System Files" : sys_files,
+	"Video" : video
 };
 var fileNames = fs.readdirSync(__dirname);
 
 for (var i = 0; i < fileNames.length; i++) {
-    var fileExtension = getExtension(fileNames[i]).toUpperCase();
+	var fileExtension = getExtension(fileNames[i]).toUpperCase();
+	for (var type in formats) {
+		if (formats.hasOwnProperty(type) && formats[type].indexOf(fileExtension) >= 0) {
+			var path = __dirname + '/' + type;
+			mkdir(path);
+			/*mv(__dirname+'/'+fileNames[i],path+'/'+fileNames[i],function(err){
+			if(err){
+			console.log("Couldn't move "+fileNames[i]+" because of following error: "+ err);
+			}
+			});*/
+		}
+	}
 	console.log(fileExtension);
 }
