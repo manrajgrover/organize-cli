5fa77bb812a16858487729991eb9ab451e54f3a1
Divjot Singh <bogas04@gmail.com>
2016-11-11 18:13:29 +0900

    use ES2015 object property shorthand
    [mdn](https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Operators/Object_initializer#Property_definitions)

--- formats.js
+++ formats.js
@@ -7,20 +7,20 @@
 
 'use strict';
 
-const audio = ["MP3", "WAV", "WMA", "MKA", "AAC", "MID", "RA", "RAM", "RM", "OGG"];
-const code = ["CPP", "RB", "PY", "HTML", "CSS", "JS"];
-const compressed = ["RAR", "JAR", "ZIP", "TAR", "MAR", "ISO", "LZ", "7ZIP", "TGZ", "GZ", "BZ2"];
-const docs = ["DOC", "DOCX", "PPT", "PPTX", "PAGES", "PDF", "ODT", "ODP", "XLSX", "XLS", "ODS", "TXT", "IN", "OUT", "MD"];
-const images = ["JPG", "JPEG", "GIF", "PNG", "SVG"];
+const Music = ["MP3", "WAV", "WMA", "MKA", "AAC", "MID", "RA", "RAM", "RM", "OGG"];
+const Codes = ["CPP", "RB", "PY", "HTML", "CSS", "JS"];
+const Compressed = ["RAR", "JAR", "ZIP", "TAR", "MAR", "ISO", "LZ", "7ZIP", "TGZ", "GZ", "BZ2"];
+const Documents = ["DOC", "DOCX", "PPT", "PPTX", "PAGES", "PDF", "ODT", "ODP", "XLSX", "XLS", "ODS", "TXT", "IN", "OUT", "MD"];
+const Images = ["JPG", "JPEG", "GIF", "PNG", "SVG"];
 const sys_files = ["DEB", "EXE", "SH", "BUNDLE"];
-const video = ["FLV", "WMV", "MOV", "MP4", "MPEG", "3GP", "MKV"];
+const Video = ["FLV", "WMV", "MOV", "MP4", "MPEG", "3GP", "MKV"];
 
 module.exports = {
-  "Music" : audio,
-  "Codes" : code,
-  "Compressed" : compressed,
-  "Documents" : docs,
-  "Images" : images,
+  Music,
+  Codes,
+  Compressed,
+  Documents,
+  Images,
+  Video,
   "System Files" : sys_files,
-  "Video" : video
 };
