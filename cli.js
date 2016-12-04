8cfc6c380c49c68bc0918e2367dfa8c7471ddcd2
Manraj Singh <manrajsinghgrover@gmail.com>
2016-12-04 23:33:37 +0530

    Refactor: CLI code uses Helper object

--- cli.js
+++ cli.js
@@ -4,26 +4,33 @@
 
 const yargs = require('yargs');
 const chalk = require('chalk');
-const helpers = require('./helpers');
+const Helpers = require('./helpers');
 const formats = require('./formats');
 
-const getExtension = helpers.getExtension,
-      getFileNames = helpers.getFileNames,
-      organize = helpers.organize;
+const helperInstance = new Helpers();
 
 const argv = yargs
   .usage('organize <command>')
   .command('it', 'Organizes current directory', (yargs) => {
-    let fileNames = getFileNames(process.cwd());
+
+    let fileNames = helperInstance.getFileNames(process.cwd());
+
     console.log(chalk.green('Scanning'));
+
     for (let i = 0; i < fileNames.length; i++) {
-      let fileExtension = getExtension(fileNames[i]).toUpperCase();
+
+      let fileExtension = helperInstance.getExtension(fileNames[i]).toUpperCase();
+
       for (let type in formats) {
+
         if (formats.hasOwnProperty(type) && formats[type].indexOf(fileExtension) >= 0) {
-          organize(process.cwd(), fileNames[i], type);
+
+          helperInstance.organize(process.cwd(), fileNames[i], type);
+
         }
       }
     }
+
     console.log(chalk.green('Done!'));
   })
   .help('h')
