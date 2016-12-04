5333bb9cd2f009434419ecc9d58033507a9d6222
Manraj Singh <manrajsinghgrover@gmail.com>
2016-12-04 23:32:39 +0530

    Refactor: Helpers refactored

--- helpers.js
+++ helpers.js
@@ -11,33 +11,38 @@ const mv = require('mv');
 const fs = require('fs');
 const path = require('path');
 
-const mkdir = (path) => {
-  try {
-    fs.mkdirSync(path);
-  } catch (err) {
-    if (err.code != 'EEXIST') {
-      throw new Error("Error occurred while creating a new directory");
+class Helpers {
+
+  constructor() { }
+
+  mkdir(path) {
+    try {
+      fs.mkdirSync(path);
+    } catch (err) {
+      if (err.code != 'EEXIST') {
+        throw new Error("Error occurred while creating a new directory");
+      }
     }
   }
-}
 
-const helpers = {
   getExtension(fileName) {
     let i = fileName.lastIndexOf('.');
     return (i < 0) ? '' : fileName.substr(i + 1);
-  },
+  }
+
   getFileNames(directory) {
     return fs.readdirSync(directory);
-  },
+  }
+
   organize(directory, fileName, type) {
     let dir = path.resolve(directory, type);
-    mkdir(dir);
+    this.mkdir(dir);
     mv(path.resolve(directory, fileName), path.resolve(dir, fileName), function (err) {
       if (err) {
         throw new Error("Couldn't move " + fileName + " because of following error: " + err);
       }
     });
   }
-};
+}
 
-module.exports = helpers;
+module.exports = Helpers;
