f95ad171ebf0db6b7e17d36754d53baf7526e343
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-01-12 22:28:19 +0530

    Fix: Linting errors in helpers.js

--- helpers.js
+++ helpers.js
@@ -4,40 +4,39 @@ const mv = require('mv');
 const fs = require('fs');
 const path = require('path');
 
-const mkdir = (path) => {
+const mkdir = (folderPath) => {
   try {
-    fs.mkdirSync(path);
+    fs.mkdirSync(folderPath);
   } catch (err) {
     if (err.code !== 'EEXIST') {
-      throw new Error("Error occurred while creating a new directory");
+      throw new Error('Error occurred while creating a new directory');
     }
   }
-}
+};
 
 const getExtension = (fileName) => {
-  let i = fileName.lastIndexOf('.');
+  const i = fileName.lastIndexOf('.');
   return (i < 0) ? '' : fileName.substr(i + 1);
-}
+};
 
-const getFileNames = (directory) => {
-  return fs.readdirSync(directory);
-}
+const getFileNames = directory => fs.readdirSync(directory);
 
 const organize = (source, output, fileName, type) => {
   mkdir(output);
 
-  let typeDir = path.resolve(output, type);
+  const typeDir = path.resolve(output, type);
   mkdir(typeDir);
 
   mv(path.resolve(source, fileName), path.resolve(typeDir, fileName), (err) => {
     if (err) {
-      throw new Error("Couldn't move " + fileName + " because of following error: " + err);
+      const errorMessage = `Couldn't move ${fileName} because of following error: ${err}`;
+      throw new Error(errorMessage);
     }
   });
-}
+};
 
 module.exports = {
   getExtension,
   getFileNames,
-  organize
-}
+  organize,
+};
