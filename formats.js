d70dce53ad89170953f3ca045eab348cdf816122
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-01-12 22:27:12 +0530

    Fix: Linting errors in formats.js

--- formats.js
+++ formats.js
@@ -1,12 +1,10 @@
-'use strict';
-
-const Music = ["MP3", "WAV", "WMA", "MKA", "AAC", "MID", "RA", "RAM", "RM", "OGG"];
-const Codes = ["CPP", "RB", "PY", "HTML", "CSS", "JS"];
-const Compressed = ["RAR", "JAR", "ZIP", "TAR", "MAR", "ISO", "LZ", "7ZIP", "TGZ", "GZ", "BZ2"];
-const Documents = ["DOC", "DOCX", "PPT", "PPTX", "PAGES", "PDF", "ODT", "ODP", "XLSX", "XLS", "ODS", "TXT", "IN", "OUT", "MD"];
-const Images = ["JPG", "JPEG", "GIF", "PNG", "SVG"];
-const Executables = ["DEB", "EXE", "SH", "BUNDLE"];
-const Video = ["FLV", "WMV", "MOV", "MP4", "MPEG", "3GP", "MKV"];
+const Music = ['MP3', 'WAV', 'WMA', 'MKA', 'AAC', 'MID', 'RA', 'RAM', 'RM', 'OGG'];
+const Codes = ['CPP', 'RB', 'PY', 'HTML', 'CSS', 'JS'];
+const Compressed = ['RAR', 'JAR', 'ZIP', 'TAR', 'MAR', 'ISO', 'LZ', '7ZIP', 'TGZ', 'GZ', 'BZ2'];
+const Documents = ['DOC', 'DOCX', 'PPT', 'PPTX', 'PAGES', 'PDF', 'ODT', 'ODP', 'XLSX', 'XLS', 'ODS', 'TXT', 'IN', 'OUT', 'MD'];
+const Images = ['JPG', 'JPEG', 'GIF', 'PNG', 'SVG'];
+const Executables = ['DEB', 'EXE', 'SH', 'BUNDLE'];
+const Video = ['FLV', 'WMV', 'MOV', 'MP4', 'MPEG', '3GP', 'MKV'];
 
 module.exports = {
   Music,
