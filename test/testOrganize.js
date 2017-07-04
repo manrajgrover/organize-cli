57b92a0f9753dcf9b053d77dac67c2a3d7583198
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-07-04 22:11:09 +0530

    Tests: Adds initial test building

--- test/testOrganize.js
+++ test/testOrganize.js
@@ -4,17 +4,19 @@ const process = require('child_process');
 const fs = require('fs-extra');
 const path = require('path');
 const { mkdir } = require('../src/helpers');
+const formats = require('../src/formats');
+const shell = require('shelljs');
 
 const TESTING_FOLDER = path.join(__dirname, '..', 'testing');
 
-const rmDir = (dirName) => {
+const removeDirsFromFolder = (dirName) => {
   let files;
   try {
     files = fs.readdirSync(dirName);
   } catch (e) {
-    console.log(`Couldn't create directory because of error: ${e}`);
-    process.exit(-1);
+    console.log(`Couldn't read directory because of error: ${e}`);
   }
+
   if (files.length > 0) {
     for (let i = 0; i < files.length; i += 1) {
       let filePath = path.join(dirName, files[i]);
@@ -28,10 +30,30 @@ const rmDir = (dirName) => {
 describe('Organize Files', () => {
   beforeEach(() => {
     mkdir(TESTING_FOLDER);
-    rmDir(TESTING_FOLDER);
+    removeDirsFromFolder(TESTING_FOLDER);
+
+    for (let folderType of Object.keys(formats)) {
+      for (let fileType of formats[folderType]) {
+        fileType = fileType.toLowerCase();
+        fs.writeFileSync(path.join(TESTING_FOLDER, `test.${fileType}`), '');
+      }
+    }
+
+    // Some Miscellaneous files
+    fs.writeFileSync(path.join(TESTING_FOLDER, 'test'), '');
+    fs.writeFileSync(path.join(TESTING_FOLDER, 'test.apib'), '');
+    fs.writeFileSync(path.join(TESTING_FOLDER, 'test.ai'), '');
+    fs.writeFileSync(path.join(TESTING_FOLDER, 'test.log'), '');
+
+    if (!shell.which('organize')) {
+      throw new Error('organize command not found');
+    }
   });
 
   it('should organize files', () => {
-    assert(1);
+    shell.exec(`organize files -s ${TESTING_FOLDER}`, (status, output) => {
+      console.log('Exit status:', status);
+      console.log('Program output:', output);
+    });
   });
 });
