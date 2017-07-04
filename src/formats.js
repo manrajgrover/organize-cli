4e0cc9fb3e4e8bb4a690ec10b8a8bca0392469c6
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-07-04 22:11:39 +0530

    Formats: Adds Php in Codes format

--- src/formats.js
+++ src/formats.js
@@ -1,5 +1,5 @@
 const Music = ['MP3', 'WAV', 'WMA', 'MKA', 'AAC', 'MID', 'RA', 'RAM', 'RM', 'OGG'];
-const Codes = ['CPP', 'RB', 'PY', 'HTML', 'CSS', 'JS'];
+const Codes = ['CPP', 'RB', 'PY', 'HTML', 'CSS', 'JS', 'PHP'];
 const Compressed = ['RAR', 'JAR', 'ZIP', 'TAR', 'MAR', 'ISO', 'LZ', '7ZIP', 'TGZ', 'GZ', 'BZ2'];
 const Documents = ['DOC', 'DOCX', 'PPT', 'PPTX', 'PAGES', 'PDF', 'ODT', 'ODP', 'XLSX', 'XLS', 'ODS', 'TXT', 'IN', 'OUT', 'MD'];
 const Images = ['JPG', 'JPEG', 'GIF', 'PNG', 'SVG'];
