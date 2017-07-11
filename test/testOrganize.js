2b86a32c7f0eb7b80adedccc252b2b03e681c29c
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-07-12 00:16:32 +0530

    Tests: Revamped code and now tests defaults

--- test/testOrganize.js
+++ test/testOrganize.js
@@ -10,6 +10,9 @@ const commandExistsSync = require('command-exists').sync;
 
 const TESTING_FOLDER = path.join(__dirname, '..', 'testing');
 
+const SOURCE_FOLDER = path.join(TESTING_FOLDER, 'source');
+const OUTPUT_FOLDER = path.join(TESTING_FOLDER, 'output');
+
 const removeDirsFromFolder = (dirName) => {
   let files;
   try {
@@ -31,12 +34,17 @@ const removeDirsFromFolder = (dirName) => {
 describe('Organize Files', () => {
   beforeEach(() => {
     mkdir(TESTING_FOLDER);
+
     removeDirsFromFolder(TESTING_FOLDER);
+
+    mkdir(SOURCE_FOLDER);
+    mkdir(OUTPUT_FOLDER);
 
     for (let folderType of Object.keys(formats)) {
       for (let fileType of formats[folderType]) {
         fileType = fileType.toLowerCase();
         fs.writeFileSync(path.join(TESTING_FOLDER, `test.${fileType}`), '');
+        fs.writeFileSync(path.join(SOURCE_FOLDER, `test.${fileType}`), '');
       }
     }
 
@@ -46,18 +54,46 @@ describe('Organize Files', () => {
     fs.writeFileSync(path.join(TESTING_FOLDER, 'test.ai'), '');
     fs.writeFileSync(path.join(TESTING_FOLDER, 'test.log'), '');
 
+    // Some Miscellaneous files in source
+    fs.writeFileSync(path.join(SOURCE_FOLDER, 'test'), '');
+    fs.writeFileSync(path.join(SOURCE_FOLDER, 'test.apib'), '');
+    fs.writeFileSync(path.join(SOURCE_FOLDER, 'test.ai'), '');
+    fs.writeFileSync(path.join(SOURCE_FOLDER, 'test.log'), '');
+
     if (!commandExistsSync('organize')) {
       throw new Error('Command "organize" command not found');
     }
   });
 
-  it('should organize files', () => {
-    syncExec(`organize -s ${TESTING_FOLDER}`);
+  it('should organize files with defaults', () => {
+    syncExec('organize');
 
     for (let folderType of Object.keys(formats)) {
       for (let fileType of formats[folderType]) {
         fileType = fileType.toLowerCase();
         assert(fs.existsSync(path.join(TESTING_FOLDER, folderType, `test.${fileType}`)));
+      }
+    }
+  });
+
+  it('should organize files with source', () => {
+    syncExec(`organize -s ${SOURCE_FOLDER}`);
+
+    for (let folderType of Object.keys(formats)) {
+      for (let fileType of formats[folderType]) {
+        fileType = fileType.toLowerCase();
+        assert(fs.existsSync(path.join(SOURCE_FOLDER, folderType, `test.${fileType}`)));
+      }
+    }
+  });
+
+  it('should organize files with source and output folder', () => {
+    syncExec(`organize -s ${SOURCE_FOLDER} -o ${OUTPUT_FOLDER}`);
+
+    for (let folderType of Object.keys(formats)) {
+      for (let fileType of formats[folderType]) {
+        fileType = fileType.toLowerCase();
+        assert(fs.existsSync(path.join(OUTPUT_FOLDER, folderType, `test.${fileType}`)));
       }
     }
   });
